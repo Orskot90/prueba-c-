a3a81e72a436e105e64081604aeb29cbcb311df5
Orskot90 <100669747+Orskot90@users.noreply.github.com>
2024-04-04 14:38:01 -0500

    Initial commit

---
+++ HolaMundo.cpp
@@ -0,0 +1,10 @@
+#include <iostream>      //libreria
+
+
+
+int main(int argc, char const *argv[])
+{
+	std::cout << "-------------------------------------------------------------------------------"  <<std::endl;
+	std::cout << "Hola mundo" <<std::endl;
+	return 0; //finalizar programa
+}
