2f32623450ba9135999434cd934fab108abda19f
Orskot90 <100669747+Orskot90@users.noreply.github.com>
2024-04-04 14:40:42 -0500

    Update HolaMundo.cpp

--- HolaMundo.cpp
+++ HolaMundo.cpp
@@ -4,7 +4,7 @@
 
 int main(int argc, char const *argv[])
 {
-	std::cout << "---------------sdfasdvc----------------------------------------------------------------"  <<std::endl;
+	std::cout << "---------------sdfasdvc--wefe--------------------------------------------------------------"  <<std::endl;
 	std::cout << "Hola mundo" <<std::endl;
 	return 0; //finalizar programa
 }
